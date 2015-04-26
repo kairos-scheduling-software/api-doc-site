01b93e415c790e16fe26bd31338a9a0de7019d04
Tony <tony.tuttle@utah.edu>
2015-04-26 10:48:38 -0600

    modified HomeController.php

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -22,4 +22,37 @@ class HomeController extends BaseController {
 		]);
 	}
 
+	public function register()
+	{
+		return View::make('api.request_key')->with([
+		       'page_name' => "REGISTER"
+		]);
+	}
+
+	public function about(){
+	  // probably good to move the about from the api and change this call -tt
+	  return View::make('api.about') -> with(['page_name' => "ABOUT"]);
+	}
+
+	public function postRegister()
+	{
+		$email = Input::get('email');
+		// verify that $email is an email
+		$validator = Validator::make(
+		  [
+		    'email' => $email
+		  ],
+		  [
+		    'email' => 'required|email'
+		  ]
+		);
+		if($validator -> fails())
+		  return Redirect::route('register') -> withErrors($validator) -> withInput(Input::all());
+		// send email address to Heroku
+		// generate email message with key and send to user
+		// display
+		// how can i send the $email variable here?
+		return View::make('api.key_sent') -> with(['page_name' => "KEYSENT",
+							   'email' => $email]);
+	}
 }
