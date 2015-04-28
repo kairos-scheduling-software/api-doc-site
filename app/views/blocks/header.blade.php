8b286ac24bc3e8e4e89ccb07b15667ab6896ca22
vince <vince@aspendigital.com>
2015-04-28 10:28:46 -0600

    Fixed logo image positioning on about page

--- app/views/blocks/header.blade.php
+++ app/views/blocks/header.blade.php
@@ -18,13 +18,13 @@
 	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 		<ul class="nav navbar-nav">
 				<li {{ $page_name == 'HOME' ? 'class="selected_page"' : '' }}>
-					<a href="{{ URL::route('home') }}">Home</a>
+					<a href="{{ URL::route('home') }}"> {{ FA::icon('home') }} Home</a>
 				</li>
 				<li {{ $page_name == 'REGISTER' ? 'class="selected_page"' : '' }}>
-					<a href="{{ URL::route('register') }}">Request Key</a>
+					<a href="{{ URL::route('register') }}">{{ FA::icon('key') }} Request Key</a>
 				</li>
 				<li {{ $page_name == 'ABOUT' ? 'class="selected_page"' : '' }}>
-					<a href="{{ URL::route('about') }}">ABOUT KAIROS</a>
+					<a href="{{ URL::route('about') }}">{{ FA::icon('info-circle') }} About Kairos</a>
 				</li>
 		</ul>
 </nav>
--- app/views/blocks/page_end.blade.php
+++ app/views/blocks/page_end.blade.php
@@ -1,5 +1,12 @@
 	<!-- Load scripts -->
     <script src="{{ URL::asset('assets/js/jquery-2.1.1.min.js') }}"></script>
+    @if($page_name == "ABOUT")
+        <script>
+        $(function(){
+            $('#logo_image').css({marginTop: '10px'});
+        });
+        </script>
+    @endif
     <script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
     <script src="{{ URL::asset('assets/js/main.js') }}"></script>
 
